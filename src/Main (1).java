9661597b4abbcc424a4199d2c55f28668a4d03c2
laustel <85309602+laustel@users.noreply.github.com>
2022-01-09 16:06:50 +0100

    @ laustel

---
+++ src/Main (1).java
@@ -0,0 +1,15 @@
+import java.awt.EventQueue;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Main {
+
+	
+	
+	public static void main(String[] args) {
+		
+			ChoixLangue choix = new ChoixLangue();
+
+	}
+}
+
